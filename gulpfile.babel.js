70338624795300892099e3e205e36927511ab484
pete hotchkiss <pete@pheekaboo.io>
2016-08-12 20:22:10 +0100

    gulp task to build SASS

---
+++ gulpfile.babel.js
@@ -0,0 +1,37 @@
+import gulp from 'gulp';
+// var jade = require('gulp-jade');
+import gulpLoadPlugins from 'gulp-load-plugins';
+import browserSync from 'browser-sync';
+import {stream as wiredep} from 'wiredep';
+
+const reload = browserSync.reload;
+const $ = gulpLoadPlugins(
+      { pattern: '*' }
+      );
+
+/*
+
+Task: styles
+Initiated as a pre-task to the minify-css task, this takes all the .scss SASS files and creates the sites css file, applying appropriate browserpre-fixes as needed. Assumes that sourcemaps are required, unless the --production flag is passed.
+*/
+// var sass = require('gulp-sass');
+gulp.task('styles', () => {
+  return gulp.src(['src/sass/main.scss'])
+    .pipe($.plumber())
+    .pipe($.sourcemaps.init())
+    // .pipe($.sass.sync({
+    //   outputStyle: 'expanded',
+    //   precision: 10,
+    //   includePaths: ['.']
+    // }).on('error', $.sass.logError) )
+    .pipe($.sass({
+      outputStyle: 'expanded',
+      precision: 10,
+      includePaths: ['.']
+    }).on('error', $.sass.logError) )
+    .pipe(gulp.dest('.tmp/css'))
+    .pipe(reload({stream: true}));
+  // return gulp.src('app/styles/main.scss')
+  //   .pipe($.sass().on('error', sass.logError))
+  //   .pipe(gulp.dest('.tmp/css'));
+});
